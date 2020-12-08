b131f48f395d7a2284e6fb1abcb208df7ef98dd1
Parth Darji <22344168+erparthqa@users.noreply.github.com>
2020-12-09 01:36:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,38 @@ const load = () => {
     };
     firebase.initializeApp(config);
 
+	var url = 'https://badasstechie.github.io/Clips/Siren.mp3';
+	window.AudioContext = window.AudioContext||window.webkitAudioContext; //fix up prefixing
+	var context = new AudioContext(); //context
+	var source ;
+
+	function call(bool)
+	{
+	    if(bool === undefined) bool = true;
+
+	    if(bool === true) 
+	    {
+		source = context.createBufferSource(); //source node
+		source.connect(context.destination); //connect source to speakers so we can hear it  
+
+		  var request = new XMLHttpRequest();
+		  request.open('GET', url, true); 
+		  request.responseType = 'arraybuffer'; //the  response is an array of bits
+		  request.onload = function() {
+		      context.decodeAudioData(request.response, function(response) {
+			  source.buffer = response;
+			   source.start(0); //play audio immediately
+			  source.loop = true;
+		      }, function () { console.error('The request failed.'); } );
+		  }
+		    request.send();  
+	    } else {
+				source.stop(context.currentTime);
+		source.disconnect(context.destination);
+		source = null;
+	    }
+	}	
+	
     const messaging = firebase.messaging();
     messaging.requestPermission().then(function () {
         return messaging.getToken();
@@ -23,40 +55,8 @@ const load = () => {
     });
 	
     messaging.onMessage(function (payload) {
-        console.log('onMessage: ', payload);
-		call();
-        
-        var url = 'https://badasstechie.github.io/Clips/Siren.mp3';
-        window.AudioContext = window.AudioContext||window.webkitAudioContext; //fix up prefixing
-        var context = new AudioContext(); //context
-        var source ;
-        
-        function call(bool)
-		{
-            if(bool === undefined) bool = true;
-
-            if(bool === true) 
-            {
-                source = context.createBufferSource(); //source node
-                source.connect(context.destination); //connect source to speakers so we can hear it  
-
-                  var request = new XMLHttpRequest();
-                  request.open('GET', url, true); 
-                  request.responseType = 'arraybuffer'; //the  response is an array of bits
-                  request.onload = function() {
-                      context.decodeAudioData(request.response, function(response) {
-                          source.buffer = response;
-                           source.start(0); //play audio immediately
-                          source.loop = true;
-                      }, function () { console.error('The request failed.'); } );
-                  }
-                    request.send();  
-            } else {
-				source.stop(context.currentTime);
-                source.disconnect(context.destination);
-                source = null;
-            }
-		}
+       	console.log('onMessage: ', payload);
+	call();
         
     });
 }
